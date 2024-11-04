af81f455b9c89cfcd1f7b141eb41bbb5d050c814
Ravinkundur <ravinkundur93@gmail.com>
2024-11-04 14:52:23 +0530

    Add files via upload

---
+++ logicaloperator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a=55,b=65;
+    if((a>12)&&(b>12)){
+        printf("a & b are greater than 12");
+    }
+else{
+    printf("a & b are not greater than 12");
+}
+}
