8ad232a606662594cf0122d144a6a5de5645ba3f
Ravinkundur <ravinkundur93@gmail.com>
2024-11-05 09:58:15 +0530

    Add files via upload

---
+++ escape.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("the first line\n and second line \n\n");
+    printf("\t hi\t");
+    printf("jack and jill\\\\\n");
+    printf("first \"in\" the \'race\'");
+    return 0;
+}
